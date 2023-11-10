b1213dbbafed01824b3bcb5707dfb59e3de6a8e2
TPGPL <TPGPL@users.noreply.github.com>
2023-11-10 23:31:46 +0100

    Add template engine

--- src/main/java/pl/edu/pw/pamiwmvc/PamiwMvcApplication.java
+++ src/main/java/pl/edu/pw/pamiwmvc/PamiwMvcApplication.java
@@ -1,7 +1,10 @@
 package pl.edu.pw.pamiwmvc;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @SpringBootApplication
 public class PamiwMvcApplication {
@@ -10,4 +13,12 @@ public class PamiwMvcApplication {
         SpringApplication.run(PamiwMvcApplication.class, args);
     }
 
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        var engine = new SpringTemplateEngine();
+
+        engine.addDialect(new LayoutDialect());
+
+        return engine;
+    }
 }
