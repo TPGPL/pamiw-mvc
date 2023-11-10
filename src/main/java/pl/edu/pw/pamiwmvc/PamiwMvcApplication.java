a0c1bc5c0c5c4fb85738af6d19680bb5c4ef44ff
TPGPL <TPGPL@users.noreply.github.com>
2023-11-10 21:52:38 +0100

    Initial commit

---
+++ src/main/java/pl/edu/pw/pamiwmvc/PamiwMvcApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.pamiwmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamiwMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PamiwMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pw/pamiwmvc/PamiwMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.pamiwmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamiwMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
