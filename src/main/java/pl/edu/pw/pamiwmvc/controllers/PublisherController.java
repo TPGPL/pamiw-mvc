89e90d75551dfa9bcf803b040f78b995c76e7bd5
TPGPL <TPGPL@users.noreply.github.com>
2023-11-11 18:34:08 +0100

    Add publishers table page

---
+++ src/main/java/pl/edu/pw/pamiwmvc/controllers/PublisherController.java
@@ -0,0 +1,27 @@
+package pl.edu.pw.pamiwmvc.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.pw.pamiwmvc.services.AuthorService;
+import pl.edu.pw.pamiwmvc.services.PublisherService;
+
+@Controller
+@RequestMapping("/publishers")
+public class PublisherController {
+    private final PublisherService service;
+
+    @Autowired
+    public PublisherController(PublisherService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String get(Model model) {
+        var publishers = service.getAll();
+        model.addAttribute("publishers", publishers);
+        return "publishers/index";
+    }
+}
