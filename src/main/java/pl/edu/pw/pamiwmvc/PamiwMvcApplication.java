734bf806db8d6b491ecc6eeb4ecf1e734f955e2f
TPGPL <TPGPL@users.noreply.github.com>
2023-11-11 17:00:28 +0100

    Remove unused imports

--- src/main/java/pl/edu/pw/pamiwmvc/PamiwMvcApplication.java
+++ src/main/java/pl/edu/pw/pamiwmvc/PamiwMvcApplication.java
@@ -1,10 +1,7 @@
 package pl.edu.pw.pamiwmvc;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @SpringBootApplication
 public class PamiwMvcApplication {
